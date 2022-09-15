34ca49fa01a6f6cb4fb2a80b2955bf09287f4662
“Marcelino” <mogt@gft.com>
2022-09-15 17:05:39 -0300

    initial commit

---
+++ backend/src/config/database.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório." 
